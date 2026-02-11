3c6c9255abc9790fcd86daa259cfefa5b01e2bb5
Gulnazym-lab <gulnazymualikhan@gmail.com>
2026-02-11 12:47:06 +0500

    Create 1.js

---
+++ lab3/task1/variables/1.js
@@ -0,0 +1,25 @@
+let message = "Hello!";
+
+//работа с переменными
+let admin;
+let name;
+
+name = "гулназым";
+admin = name;
+
+alert(admin);
+
+//дать правильное имя
+
+const ourcountryname = "kazakhstan";
+let kzname = "zhangir";
+console.log(ourcountryname);
+console.log(kzname)
+ // болльшой буквы 
+ const TUGANKuN = "18.04.1982";
+const jasy= someCode(TUGANKuN);
+
+function someCode(birthdayStr) {
+  return 42;
+}
+console.log(jasy);
