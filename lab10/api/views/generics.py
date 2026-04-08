0ff4beb10739fa66b822f9f0f01d811fb6a4d02f
Gulnazym-lab <gulnazymualikhan@gmail.com>
2026-04-08 11:17:07 +0500

    Add files via upload

---
+++ lab10/api/views/generics.py
@@ -0,0 +1,40 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from api.models import Product, Category
+from api.serializers import ProductSerializer, CategorySerializer
+
+
+class ProductListAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+    lookup_field = 'id'
+
+
+class CategoryListAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_url_kwarg = 'category_id'
+    lookup_field = 'id'
+
+
+class CategoryProductsAPIView(APIView):
+    def get(self, request, category_id):
+        category = get_object_or_404(Category, id=category_id)
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
